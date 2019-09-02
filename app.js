ec95c38d07a90eecb5cac91ce5c80519f9447079
Lovel Bangura <abangura14@yahoo.com>
2019-09-02 13:44:56 +0000

    delete comment

--- app.js
+++ app.js
@@ -36,44 +36,6 @@ app.use(require('./app/routes/mainRouter'), function(req, res, next) {
     next();
 });
 
-
-
-//retreiving data from the csv file
-// var csvStream = csv.parseFile(".\\excell\\Final_file_2.csv", { headers: true })
-//     .on("data", function(record) {
-//         // pausing before checking
-//         csvStream.pause();
-
-//         if (counter < 2664) {
-//             var siteNum = record.S_N;
-//             var Lat = record.LAT;
-//             var Long = record.LONG;
-//             var Mcc = record.MCC;
-//             var Mnc = record.MNC;
-//             var nearestFacility = record.NEAREST_FACILITY;
-//             var siteId = record.Site_ID;
-//             var siteRName = record.Site_RName;
-//             var cellId = record.Cell_ID;
-//             var cellName = record.Cell_Name;
-
-//             // inserting data to the database
-//             pool.query("INSERT INTO find_nearest_hf(siteNum, Lat, Long,Mcc,Mnc,nearestFacility,siteId,siteRName,cellId, cellName) \
-//                     VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [siteNum, Lat, Long, Mcc, Mnc, nearestFacility, siteId, siteRName, cellId, cellName], function(err) {
-//                 if (err) {
-//                     console.log(err);
-//                 }
-//             });
-//             ++counter;
-//         }
-//         // file storing complete
-//         csvStream.resume();
-//         // callbackfunction
-//     }).on("end", function() {
-//         console.log("Done.....");
-//     }).on("error", function(err) {
-//         console.log(err);
-//     })
-
 //listening to the 3000 port
 var server = app.listen(app.get('port'), function() {
     console.log('listening on port ' + app.get('port'));
